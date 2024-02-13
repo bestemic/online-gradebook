809329751cfdf9f8743e76586900aef57cd8f667
bestemic <przemek.pawlik123@gmail.com>
2024-02-13 17:44:02 +0100

    feat: generate initial backend project

---
+++ backend/src/test/java/com/bestemic/onlinegradebook/OnlineGradebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestemic.onlinegradebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineGradebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
