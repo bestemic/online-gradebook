eff8da937325b0a84d04c39034dfdc15c445e399
bestemic <przemek.pawlik123@gmail.com>
2024-09-07 16:25:39 +0200

    fix: remove labels from cloud sql instance

--- terraform/database.tf
+++ terraform/database.tf
@@ -2,6 +2,5 @@ resource "google_sql_database_instance" "database-instance" {
   project          = var.project_id
   name             = "${var.app-name}-database-instance"
   region           = var.region
-  labels           = local.labels
   database_version = "MYSQL_8_0"
 }
