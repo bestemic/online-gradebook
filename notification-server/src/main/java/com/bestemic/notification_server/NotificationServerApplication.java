6211274f5d5da3a3d2dfb5f28fff06e4451c4c52
bestemic <przemek.pawlik123@gmail.com>
2024-09-08 12:28:02 +0200

    feat: add health check

--- notification-server/src/main/java/com/bestemic/notification_server/NotificationServerApplication.java
+++ notification-server/src/main/java/com/bestemic/notification_server/NotificationServerApplication.java
@@ -4,14 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @EnableScheduling
 @SpringBootApplication
+@RestController
 public class NotificationServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotificationServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServerApplication.class, args);
+    }
 
+    @GetMapping("/")
+    public String healthCheck() {
+        return "OK";
+    }
 }
