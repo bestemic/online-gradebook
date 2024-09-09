3de29ba2dd9347fa8d948d39c1b44925ea5df664
bestemic <przemek.pawlik123@gmail.com>
2024-09-09 18:56:41 +0200

    feat: add user initialization in workflow

--- backend/run-app.sh
+++ backend/run-app.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+echo "ADD_USER=${ADD_USER}"
+echo "FIRST_NAME=${FIRST_NAME}"
+echo "LAST_NAME=${LAST_NAME}"
+echo "EMAIL=${EMAIL}"
+echo "PASSWORD=${PASSWORD}"
+echo "ROLES=${ROLES}"
+echo "PHONE_NUMBER=${PHONE_NUMBER}"
+echo "BIRTH=${BIRTH}"
+
 APP_ARGS=""
 
 if [ "$ADD_USER" == "true" ]; then
-  echo "Adding user..."
   APP_ARGS+=" --firstName=${FIRST_NAME}"
   APP_ARGS+=" --lastName=${LAST_NAME}"
   APP_ARGS+=" --email=${EMAIL}"
   APP_ARGS+=" --password=${PASSWORD}"
+  APP_ARGS+=" --roles=${ROLES}"
   APP_ARGS+=" --phoneNumber=${PHONE_NUMBER}"
   APP_ARGS+=" --birth=${BIRTH}"
-  APP_ARGS+=" --roles=${ROLES}"
 fi
 
 exec java -jar app.jar --spring.profiles.active=${SPRING_PROFILES_ACTIVE} ${APP_ARGS}
